60b763afa7f255b50229e55e5615003409277a43
Jeffery Thompson <jlthompson3259@gmail.com>
2021-02-11 16:05:37 -0500

    Update Healthcheck.cs

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Tag Healthy";
+            Status = "Login Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
